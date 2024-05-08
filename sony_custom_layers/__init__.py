07cac23a9e25f6280dbf1c7e34a6dab3aa6561f6
irenab <Irena.Byzalov@altair-semi.com>
2024-05-08 09:55:18 +0300

    check dynamic requirements by import rather than by pip package

--- sony_custom_layers/__init__.py
+++ sony_custom_layers/__init__.py
@@ -15,14 +15,16 @@
 # -----------------------------------------------------------------------------
 
 # minimal requirements for dynamic validation in sony_custom_layers.{keras, pytorch}.__init__
-requirements = {
+# library names are the names that are used in import statement rather than pip package name, as a library can have
+# multiple providing packages per arch, device etc.
+required_libraries = {
     'tf': ['tensorflow>=2.10'],
     'torch': ['torch>=2.0', 'torchvision>=0.15'],
     'torch_ort': ['onnx', 'onnxruntime', 'onnxruntime_extensions>=0.8.0'],
 }
 
 # pinned requirements of latest tested versions for extra_requires
-pinned_requirements = {
+pinned_pip_requirements = {
     'tf': ['tensorflow==2.15.*'],
     'torch': ['torch==2.2.*', 'torchvision==0.17.*'],
     'torch_ort': ['onnx==1.15.*', 'onnxruntime==1.17.*', 'onnxruntime_extensions==0.10.*']
